0405acfaa1257cbdca48c15aeb1f5198fbe7b1fd
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-08-03 11:05:32 -0700

    Code to check logging and exception added

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+## Below is the code to check if the logging module is working correctly.
+
 # from src.logger import logging
 
 # logging.debug("This is a debug message")
@@ -5,3 +7,16 @@
 # logging.warning("This is a warning message")
 # logging.error("This is an error message")
 # logging.critical("This is a critical message")
+
+## Below is the code to check if the custom exception class is working correctly.
+
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
+
+# try:
+#     # Simulating an error
+#     a = 1 / 0
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
