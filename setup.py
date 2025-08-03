ef0215715a74344c88d5c6a7902ca25df5f496c4
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-08-03 08:39:45 -0700

    setup.py and project.toml code added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Atharva Chundurwar',
+    author_email='atharvachundurwar841@gmail.com',
+    packages=find_packages(),
+)
