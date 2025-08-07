48cea78810cadcec906e4a0a88b494729cdf3b2a
atharvaBOT7 <atharvachundurwar841@gmail.com>
2025-08-07 14:35:29 -0700

    App completely ready locally

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -70,3 +70,8 @@ class ModelPusherConfig:
     load_dotenv()
     bucket_name: str = MODEL_BUCKET_NAME
     s3_model_key_path: str = MODEL_FILE_NAME
+
+@dataclass
+class VehiclePredictorConfig:
+    model_file_path: str = MODEL_FILE_NAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
